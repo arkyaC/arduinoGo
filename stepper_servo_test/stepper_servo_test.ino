1fa3a80357c1419c6c22b0f67e1ee8f57abaa1ad
arkyaC <arkya.chat@gmail.com>
2017-10-25 16:36:56 +0530

    servo and stepper first-level integration
    Servo is only screwing up when powering on, remove it from its position
    when connecting

--- stepper_servo_test/stepper_servo_test.ino
+++ stepper_servo_test/stepper_servo_test.ino
@@ -8,9 +8,8 @@ int last_pos = 165;    // variable to store the servo position
 int pos;
 
 
-const int stepsPerRevolution =200;// change this to fit the number of steps per revolution
-// for your motor
-const int numberOfRev =20;// this is as per the experiment
+const int stepsPerRevolution =200;// DON'T CHANGE
+int numberOfRev;// this is where USER INPUT goes (for now)
 
 // initialize the stepper library on pins 8 through 11:
 Stepper myStepper(stepsPerRevolution, 8, 9, 10, 11);
@@ -20,23 +19,37 @@ void setup() {
   myStepper.setSpeed(60);
   // initialize the serial port:
   Serial.begin(9600); 
+  //myservo.attach(3);
   // step one revolution  in one direction:`
-  Serial.println("clockwise");
+  //Serial.println("clockwise");
 
  // myStepper.step(stepsPerRevolution);
-  for (int i=0; i<numberOfRev; i++){
-    myStepper.step(-200);
-    Serial.print("steps:");
-    Serial.println(i+1);
-    delay(10);
-    digitalWrite(8, LOW);
-    digitalWrite(9, LOW);
-    digitalWrite(10, LOW);
-    digitalWrite(11, LOW);
-    delay(250);
-  }
   
-  }
+}
   void loop(){
-  
+    if(Serial.available()>0){
+      pos=Serial.parseInt();
+    }
+    if(pos>=155){
+      myservo.attach(3);
+      myservo.write(pos);
+      Serial.println(pos);
+      last_pos=pos;
+      delay(200);
+      myservo.detach();
+    }
+    else if(pos<30){
+      numberOfRev=pos;
+      for (int i=0; i<numberOfRev; i++){
+        myStepper.step(200);//DIRECTION INFORMATION goes here
+        Serial.print("steps:");
+        Serial.println(i+1);
+        delay(10);
+        digitalWrite(8, LOW);
+        digitalWrite(9, LOW);
+        digitalWrite(10, LOW);
+        digitalWrite(11, LOW);
+        delay(250);
+      }
+    }
 }
